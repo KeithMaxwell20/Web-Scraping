694e25602d4a720dfb91c258a67fa96a90343968
KeithMaxwell20 <80549311+KeithMaxwell20@users.noreply.github.com>
2022-05-01 10:35:04 -0400

    1.3 concluido; 1.4 en proceso
    Falta definir la función para ordenar por rating y ordenar las 3 listas para el 1.5

--- Python/main.py
+++ Python/main.py
@@ -48,11 +48,29 @@ def crear_csv(lista_lenguajes, cantidad_repositorios):
     escritor = csv.writer(archivo) #Escritor para el archivo csv
 
     # Escribimos los nombres de los lenguajes junto a su cantidad de repositorios
-    escritor.writerow(lista_lenguajes)
+
     escritor.writerow(cantidad_repositorios)
+    escritor.writerow(lista_lenguajes)
 
     print("El archivo csv se ha creado con éxito")
 
+# Retornar una lista con el rating de los lenguajes de programación
+def retornar_rating(min, max, cantidad_repositorios):
+    lista_rat = []
+    for elemento in cantidad_repositorios:
+        rating = (elemento - min)*100/(max-min)
+        lista_rat.append(rating)
+
+    return lista_rat
+
+def ordenar_imprimir(lista):
+    # Ordenar lista
+    lista_ordenada = lista.sort(reverse=True)
+    #Imprimir lista
+
+    # El problema de devolver la lista en formato zip es que necesito para
+    # graficar, a menos que pueda obtener su índice
+    return lista_ordenada
 
 '''Este es un programa que utiliza Web Scraping para analizar y comparar la
 popularidad de distintos lenguajes de programación según el índice Tiobe'''
@@ -66,23 +84,34 @@ lista_populares = ["Python", "C", "Java", "C++", "C#", "Visual Basic", "JavaScri
 # Lenguajes por su URL en Github
 lista_topic = ["python", "c", "java", "cpp", "csharp", "visual-basic", "javascript",
 		       "assembly", "sql", "php", "r", "delphi", "go", "swift", "ruby",
-               "visual-basic", "objective-c", "perl", "lua", "matlab"]
+               "visual-basic-for-applications", "objective-c", "perl", "lua", "matlab"]
 
 
 # 1.1 Extraer el número de repositorios de los lenguajes populares
 # Dirección de la página
 url = "https://github.com/topics"
 
+# ¿Es necesario elegir entre leer el archivo y ejecutar las funciones?
+
 # Verificamos si el archivo con los tópicos y sus repositorios existe
-if not os.path.exists('/resultados.csv'):
-    # Recibimos la lista con los tópicos por cada lenguaje
-    lista = retornar_lista(url, lista_topic)
-    # 1.2 Creamos el archivo csv
-    crear_csv(lista, lista_populares) # Se debe añadir nombres a las filas
+#if not os.path.exists('/resultados.csv'):
+# Recibimos la lista con los tópicos por cada lenguaje
+lista = retornar_lista(url, lista_topic)
+# 1.2 Creamos el archivo csv
+crear_csv(lista, lista_populares)
+#else: # Leemos el archivo csv para obtener la lista
 
-#1.3
 
+#1.3 Hallar máximo y mínimo número de apariciones
+# (puede ser convertido a una función que retorne los 2 valores)
+minimo = min(lista)
+maximo = max(lista)
 
+#Calculamos el rating de github y lo colocamos en una lista
+lista_rating = retornar_rating(minimo, maximo, lista)
+
+# 1.4 Ordenar descendentemente e imprimir
+lista_rating = ordenar_imprimir(lista_rating)
 
 
 
