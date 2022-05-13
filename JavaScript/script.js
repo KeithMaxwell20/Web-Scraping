e43eea41f7e7d7e9cf0677806f07cbf49fca2582
Victor Ayala <87955076+VikMannu@users.noreply.github.com>
2022-05-13 17:04:44 -0400

    script.js para la gráfica del html

---
+++ JavaScript/script.js
@@ -0,0 +1,43 @@
+const requestURL = 'data.json';
+const request = new XMLHttpRequest();
+
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function () {
+
+    const dataJSON = request.response
+
+    console.log(dataJSON)
+
+    // Obtener una referencia al elemento canvas del DOM
+    const $grafica = document.querySelector("#grafica");
+
+    new Chart($grafica, {
+        type: 'bar',// Tipo de gráfica
+        data: {
+            labels: dataJSON['NOMBRE_LENGUAJE'],
+            datasets: [{
+                backgroundColor: "#42C2FF",
+                data: dataJSON['NRO_APARICIONES']
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }],
+            },
+            legend: { display: false },
+            title: {
+                display: true,
+                text: "Gráficas de barras",
+                fontSize: 24
+            }
+        }
+    });
+
+}
